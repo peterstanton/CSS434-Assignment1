84c1d91aff50ced382f3db9b23f89259dddf5de7
Peter <stantp2@uw.edu>
2017-04-16 16:47:07 -0700

    loop everything so exceptions don't pull us out of the loop.

--- src/Server.java
+++ src/Server.java
@@ -22,52 +22,50 @@ public Server(int port) {
            }
             catch(IOException ioe) {  //stuff here.
             }
-            try {
-                serverSideSocket.setSoTimeout(500);
-            }
-            catch (SocketException se) {
-            }
+            while (true) {
+                try {
+                    serverSideSocket.setSoTimeout(500);
+                } catch (SocketException se) {
+                }
 
-            try {
-                while (true) {
-                    serverSideSocket.accept();
-                    if (serverSideSocket != null) {
-                        Socket clientSocket;
-                        clientSocket = serverSideSocket.accept();
-                        Connection detected = new Connection(clientSocket);
-                        conList.add(detected);
-                        for (int i = 0; i < conList.size(); i++) {
-                            //if a client has a message.
-                            if (conList.get(i).getAvailable()) {
-                                //get the message and send it on.
-                                String myText = conList.get(i).getMessage();    //I should really obey encapsulation and interact using methods.
-                                String senderID = conList.get(i).getID();
-                                //send the data to the other connections.
-                                for (int j = 0; j < conList.size(); j++) {
-                                    if (!senderID.equals(conList.get(j).getID())) {  //replace with equals()?  if (conList.get(j).userID != senderID)
-                                        //write the outgoing message to other users.
-                                        conList.get(j).sendMessage(myText);
+                try {
+                    while (true) {
+                        serverSideSocket.accept();
+                        if (serverSideSocket != null) {
+                            Socket clientSocket;
+                            clientSocket = serverSideSocket.accept();
+                            Connection detected = new Connection(clientSocket);
+                            conList.add(detected);
+                            for (int i = 0; i < conList.size(); i++) {
+                                //if a client has a message.
+                                if (conList.get(i).getAvailable()) {
+                                    //get the message and send it on.
+                                    String myText = conList.get(i).getMessage();    //I should really obey encapsulation and interact using methods.
+                                    String senderID = conList.get(i).getID();
+                                    //send the data to the other connections.
+                                    for (int j = 0; j < conList.size(); j++) {
+                                        if (!senderID.equals(conList.get(j).getID())) {  //replace with equals()?  if (conList.get(j).userID != senderID)
+                                            //write the outgoing message to other users.
+                                            conList.get(j).sendMessage(myText);
+                                        }
                                     }
                                 }
                             }
-                        }
-                        for (int k = 0; k < conList.size(); k++) {
-                            if(conList.get(k).getStatus() == true) {
-                                conList.remove(k);
+                            for (int k = 0; k < conList.size(); k++) {
+                                if (conList.get(k).getStatus() == true) {
+                                    conList.remove(k);
+                                }
                             }
+                            //checking to see if we got a connection.
+                        } else {
+                            continue;
+                            //I don't know.
                         }
-                        //checking to see if we got a connection.
-                    } else {
-                        continue;
-                        //I don't know.
                     }
+                } catch (IOException ioe) {  //stuff here.
                 }
-            }
-            catch(IOException ioe)
-            {  //stuff here.
-            }
 
-
+            }
     }
     public static void main( String[] args ) {
         int listeningPort=Integer.parseInt(args[2]); //for debugging.
