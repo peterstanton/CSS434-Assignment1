4df040c093ec8e5ab9a28a13d6d1a03b11cc300e
Peter Stanton <peterstanton@users.noreply.github.com>
2017-04-16 18:18:47 -0700

    Add files via upload
    Functional server file.

--- Server.java
+++ Server.java
@@ -14,58 +14,45 @@ public class Server {
     private LinkedList<Connection> conList;
 
 
-public Server(int port) {
+public Server(int port) throws IOException {
+	conList = new LinkedList<Connection>();
+	System.out.println("I set the Socket");
+        serverSideSocket = new ServerSocket(port);
+	System.out.println("Hi");
+        serverSideSocket.setSoTimeout(500);
+        while (true) {
 
-	   conList = new LinkedList<Connection>();
-           try {
-               serverSideSocket = new ServerSocket(port); //this all needs to be in the loop.
-           }
-            catch(IOException ioe) {  //stuff here.
-            }
-            while (true) {
-                try {
-                    serverSideSocket.setSoTimeout(500);
-                } catch (SocketException se) {
-                  System.out.println("Nothing found");
-                }
 
                 try {
                     while (true) {
-			            Socket clientSocket;
-			            clientSocket = serverSideSocket.accept();
+                        Socket clientSocket;
+                        clientSocket = serverSideSocket.accept();
                         if (clientSocket != null) {
-				            System.out.println("Socket found.");
                             Connection detected = new Connection(clientSocket);
                             conList.add(detected);
-				            System.out.println("Connection Added.");
-
-                            //checking to see if we got a connection.
-                        } else {
-                            System.out.println("Nothing");
-                            //I don't know.
-                        }
-			            for (int i = 0; i < conList.size(); i++) {
-				                System.out.println("Checking my connections");
+                            for (int i = 0; i < conList.size(); i++) {
                                 //if a client has a message.
                                 if (conList.get(i).getAvailable()) {
-					                System.out.println("I have a message!");
                                     //get the message and send it on.
-                                    String myText = conList.get(i).getMessage();    //I should really obey encapsulation and interact using methods.
+                                    String myText = conList.get(i).getMessage();    
                                     String senderID = conList.get(i).getID();
                                     //send the data to the other connections.
                                     for (int j = 0; j < conList.size(); j++) {
-                                        if (!senderID.equals(conList.get(j).getID())) {  //replace with equals()?  if (conList.get(j).userID != senderID)
+                                        if (!senderID.equals(conList.get(j).getID())) { 
                                             //write the outgoing message to other users.
                                             conList.get(j).sendMessage(myText);
                                         }
                                     }
                                 }
                             }
-
-                        for (int k = 0; k < conList.size(); k++) {
-                            if (conList.get(k).getStatus() == true) {
-                                conList.remove(k);
+                            for (int k = 0; k < conList.size(); k++) {
+                                if (conList.get(k).getStatus() == true) {
+                                    conList.remove(k);
+                                }
                             }
+                            //checking to see if we got a connection.
+                        } else {
+
                         }
                     }
                 } catch (IOException ioe) {  //stuff here.
@@ -73,9 +60,8 @@ public Server(int port) {
 
             }
     }
-    public static void main( String[] args ) {
-        int listeningPort=Integer.parseInt(args[0]); //for debugging.
-        // int listeningPort = 1245;  //this overflows in debugging.
+    public static void main( String[] args ) throws IOException {
+        int listeningPort=Integer.parseInt(args[0]);
         Server s = new Server(listeningPort);
         System.out.println("Hello World!");
     }
