8dbf9d4090452d9c1ee707a4e0de9c44b147aaa7
Peter Stanton <peterstanton@users.noreply.github.com>
2017-04-17 10:51:17 -0700

    Add files via upload
    Nicely commented code, remove debug code.

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,11 @@
 /**
  * Created by Peter on 3/27/2017.
+// Class Server
+// This class provides a server for basic TCP
+// text chat using strings between 2 or more clients
+// Text input output is handled by a provided ChatClient.
+// This class contains a server socket to listen to connections
+// and a list to contain active connections.
  */
 
 import java.io.*;
@@ -16,22 +22,20 @@ public class Server {
 
 public Server(int port) throws IOException {
 	conList = new LinkedList<Connection>();
-	System.out.println("I set the Socket");
         serverSideSocket = new ServerSocket(port);
-	System.out.println("Hi");
         serverSideSocket.setSoTimeout(500);
         while (true) {
                 try {
                     while (true) {
                         Socket clientSocket;
-                        clientSocket = serverSideSocket.accept();
+                        clientSocket = serverSideSocket.accept(); //set client socket.
                         if (clientSocket != null) {
-                            Connection detected = new Connection(clientSocket);
-                            conList.add(detected);
+                            Connection detected = new Connection(clientSocket); //instantiate connection
+                            conList.add(detected); //add client to list.
                             //checking to see if we got a connection.
-                            runConnections();
+                            runConnections(); //process the connections.
                         } else {
-                            runConnections();
+                            runConnections(); //we have no new connections, but run the ones we have
                         }
                     }
                 } catch (IOException ioe) {
@@ -41,7 +45,9 @@ public Server(int port) throws IOException {
 
             }
     }
-
+//Run connections moves through our current list of connections and checks them for messages
+//and if any are detected, propagates them to the other clients. It also checks for errored
+//clients and removes them from the connection list.
     public void runConnections() {
         for (int i = 0; i < conList.size(); i++) {
             //if a client has a message.
@@ -51,16 +57,16 @@ public Server(int port) throws IOException {
                 String senderID = conList.get(i).getID();
                 //send the data to the other connections.
                 for (int j = 0; j < conList.size(); j++) {
-                    if (!senderID.equals(conList.get(j).getID())) {
+                    if (!senderID.equals(conList.get(j).getID())) { //do not send message to myself.
                         //write the outgoing message to other users.
                         conList.get(j).sendMessage(myText);
                     }
                 }
             }
-        }
+        } //checks for lapsed user connections.
         for (int k = 0; k < conList.size(); k++) {
             if (conList.get(k).getStatus() == true) {
-                conList.remove(k);
+                conList.remove(k); //remove them.
             }
         }
 
@@ -68,7 +74,6 @@ public Server(int port) throws IOException {
     public static void main( String[] args ) throws IOException {
         int listeningPort=Integer.parseInt(args[0]);
         Server s = new Server(listeningPort);
-        System.out.println("Hello World!");
     }
 
 }
